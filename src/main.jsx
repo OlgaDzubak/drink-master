99fbab8131a61bf1cb3f12bad1a5fd883db04c27
Olga Dzubak <iamAndriy80@gmail.com>
2025-03-27 16:31:07 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../src/redux/store.js';
 import { Global } from '@emotion/react';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import GlobalContextProvider from "./context/GlobalContext";
 import GlobalStyles from './GlobalStyled.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/drinkMaster-Team-1">
+    <BrowserRouter basename="https://olgadzubak.github.io/drink-master_frontend">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <GlobalContextProvider>
