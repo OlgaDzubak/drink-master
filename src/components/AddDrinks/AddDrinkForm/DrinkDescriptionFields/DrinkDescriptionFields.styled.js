4ceb5738b91bcec081d7f448b75b431479d4db29
Olga Dzubak <iamAndriy80@gmail.com>
2025-05-14 18:51:17 +0300

    Update DrinkDescriptionFields.styled.js

--- src/components/AddDrinks/AddDrinkForm/DrinkDescriptionFields/DrinkDescriptionFields.styled.js
+++ src/components/AddDrinks/AddDrinkForm/DrinkDescriptionFields/DrinkDescriptionFields.styled.js
@@ -22,7 +22,7 @@ export const DescriptionImageDiv = styled.div`
   position: relative;
   width: 100%;
   height: 320px;
-  margin-bottom: 40px;
+  margin-bottom: 5px;
   display: flex;
   flex-direction: column;
   align-items: center;
