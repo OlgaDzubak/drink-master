3ac913e90e3ea7b5b3a1520d07a88fb9e96befad
OlgaDzubak <118764742+OlgaDzubak@users.noreply.github.com>
2025-05-14 15:51:47 +0000

    Deploying to gh-pages from @ OlgaDzubak/drink-master@4ceb5738b91bcec081d7f448b75b431479d4db29 🚀

---
+++ assets/PageTitle-8594c1a0.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-f71a6a39.js";import{a as s}from"./coctailPlaceholder-5290f01d.js";const m=({title:r,hidden:t,marginBottom:e})=>o.jsx(s,{hidden:t,marginBottom:e,children:r});export{m as P};
---
+++ assets/SectionTitle-c1774361.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-f71a6a39.js";import{T as i}from"./coctailPlaceholder-5290f01d.js";const n=({title:t,hidden:r,marginBottom:o})=>e.jsx(i,{hidden:r,marginBottom:o,children:t});export{n as S};
