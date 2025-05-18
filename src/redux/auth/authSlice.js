42e5cad66499b8d102cafd9aeef2e7f113bbbfad
Olga Dzubak <iamAndriy80@gmail.com>
2025-05-18 08:47:28 +0300

    Update authSlice.js

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -65,13 +65,14 @@ const handlePending_refresh = (state) => {
 
 const handleFulfilled_signup = (state, action) => {
   state.user = action.payload.user;
+  state.token = action.payload.token;
   state.isLoading = false;
   state.error = null;
   state.isEmailVerificationModalOpen = true;
 };
 
 const handleFulfilled_verify = (state, action) => {
-  Notify.info('Email verified', { position: "right-top" });
+  Notify.success('Email verified', { position: "right-top" });
   state.isEmailVerificationModalOpen = false;
   state.user.verify = true;
   state.isLoggedIn = true;
